476f799dee626a3aa67e288d3f39601ca69b41de
Николай Чечерин <checherin_na@sima-land.ru>
2024-08-14 20:49:25 +0500

    Выполнение четвертого задания

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 
 func main() {
+
+	var validNames [5]string
+	validNames[0] = "Николай"
+	validNames[1] = "Егор"
+	validNames[2] = "Иван"
+	validNames[3] = "Андрей"
+	validNames[4] = "Тормунд"
+
 	var greeting string
 	fmt.Print("Введите ваше имя: ") 
 	fmt.Scan(&greeting)
-	if greeting == "Николай" {        
-		fmt.Println("Рады вас приветствовать, " + greeting + "!")
-	} else {
-		fmt.Println("Пошёл на хуй, " + greeting)
+
+	for i := 0; i < len(validNames); i++ {
+		if greeting == validNames[i] {
+			fmt.Println("Рады вас приветствовать, " + greeting + "!")
+			return
+		}
 	}
+	fmt.Println("Пошёл на хуй, " + greeting)
 }
