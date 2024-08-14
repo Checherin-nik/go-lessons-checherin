55578304a271336fbb3505efa6dc0adbaa647012
Николай Чечерин <checherin_na@sima-land.ru>
2024-08-14 19:59:51 +0500

    Выполнение третьего задания

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	var greeting string
 	fmt.Print("Введите ваше имя: ") 
-	fmt.Scanln(&greeting)        
-	fmt.Println("Рады вас приветствовать, " + greeting + "!")
+	fmt.Scan(&greeting)
+	if greeting == "Николай" {        
+		fmt.Println("Рады вас приветствовать, " + greeting + "!")
+	} else {
+		fmt.Println("Пошёл на хуй, " + greeting)
+	}
 }
