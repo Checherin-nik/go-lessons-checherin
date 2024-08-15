bb991bd45033bc7d795e4c75e8e345eec5c3dd30
Николай Чечерин <checherin_na@sima-land.ru>
2024-08-15 20:23:55 +0500

    Рефакториг функции trueUser, введение слайса в массив validNames

--- main.go
+++ main.go
@@ -2,28 +2,26 @@ package main
 
 import "fmt"
 
-func trueUser(name string, validNames [5]string) {
+func trueUser(name string, validNames []string) bool {
 	for _, validName := range validNames {
 		if name == validName {
-			fmt.Println("Рады вас приветствовать, " + name + "!")
-			return
+			return true
 		}
 	}
-	fmt.Println("Пошёл на хуй, " + name)
+	return false
 }
 
 func main() {
 
-	var validNames [5]string
-	validNames[0] = "Николай"
-	validNames[1] = "Егор"
-	validNames[2] = "Иван"
-	validNames[3] = "Андрей"
-	validNames[4] = "Тормунд"
+	validNames := []string{"Николай", "Егор", "Иван", "Андрей", "Тормунд"}
 
 	var greeting string
 	fmt.Print("Введите ваше имя: ") 
 	fmt.Scan(&greeting)
 
-	trueUser(greeting, validNames)
+	if trueUser(greeting, validNames) {
+		fmt.Println("Рады вас приветствовать, " + greeting + "!")
+	} else {
+		fmt.Println("Пошёл на хуй, " + greeting)
+	}
 }
