45ff849454365ac4e2c9fa8198f5e8f51106e201
Николай Чечерин <checherin_na@sima-land.ru>
2024-08-20 20:18:11 +0500

    Функция переделана в соответствии с happy left path

--- main.go
+++ main.go
@@ -19,33 +19,34 @@ func main() {
 	fmt.Print("Введите ваше имя: ") 
 	fmt.Scan(&greeting)
 
-	if trueUser(greeting, validNames) {
-		fmt.Println("Рады вас приветствовать, " + greeting + "!")
+	if !trueUser(greeting, validNames) {
+		fmt.Println("Пошёл на хуй, " + greeting)
+		return
+	}
 
-		for {
-			fmt.Print("Введите команду: ")
-			var command string
-			fmt.Scan(&command)
+	fmt.Println("Рады вас приветствовать, " + greeting + "!")
 
-			if command == "exit" {
-				fmt.Println("Вы закончили, " + greeting)
-				break
-			} else if command == "print" {
-				fmt.Println("Список доступных имён: " + strings.Join(validNames, ", "))
-			} else if command == "add" {
-				fmt.Print("Введите имя, которое хотите добавить: ")
-				var newName string
-				fmt.Scan(&newName)
+	for {
+		fmt.Print("Введите команду: ")
+		var command string
+		fmt.Scan(&command)
 
-				if trueUser(newName, validNames) {
-					fmt.Println("Имя уже существует в списке.")
-				} else {
-					validNames = append(validNames, newName)
-					fmt.Println("Имя " + newName + " добавлено в список.")
-				}
+		if command == "exit" {
+			fmt.Println("Вы закончили, " + greeting)
+			break
+		} else if command == "print" {
+			fmt.Println("Список доступных имён: " + strings.Join(validNames, ", "))
+		} else if command == "add" {
+			fmt.Print("Введите имя, которое хотите добавить: ")
+			var newName string
+			fmt.Scan(&newName)
+
+			if trueUser(newName, validNames) {
+				fmt.Println("Имя уже существует в списке.")
+			} else {
+				validNames = append(validNames, newName)
+				fmt.Println("Имя " + newName + " добавлено в список.")
 			}
 		}
-	} else {
-		fmt.Println("Пошёл на хуй, " + greeting)
 	}
-}
+} 
